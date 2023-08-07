d509b43a35ab041b6b687a570d5e2966ef90bede
tannazma <97254921+tannazma@users.noreply.github.com>
2023-08-07 23:25:25 +0200

    make toggle dark button with picture and make it more beautiful

--- script.js
+++ script.js
@@ -2,11 +2,14 @@
 function darkMode() {
     let element = document.body;
     element.classList.toggle("dark-mode");
-    const darkButton = document.getElementById("darkMode");
-    darkButton.className = "light-mode";
-    darkButton.textContent = "🌛";
-    if (element.classList.contains("dark-mode")){
-        darkButton.textContent = "☀️";  
-    };
+
+    if (element.classList.contains("dark-mode")) {
+        const darkButton = document.querySelector(".light-mode-icon");
+        darkButton.src = "/public/light-theme.png";
+    }
+    else {
+        const lightButton = document.querySelector(".light-mode-icon");
+        lightButton.src = "/public/night-theme.png";
+    }
 
 }
