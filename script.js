551093e494cbd0b99b5587e230877abf4d7e34a5
tannazma <97254921+tannazma@users.noreply.github.com>
2023-08-11 15:18:39 +0200

    make dark mode button persistant

--- script.js
+++ script.js
@@ -1,15 +1,21 @@
-
-function darkMode() {
+function toggleTheme() {
     let element = document.body;
     element.classList.toggle("dark-mode");
 
     if (element.classList.contains("dark-mode")) {
         const darkButton = document.querySelector(".light-mode-icon");
         darkButton.src = "/public/light-theme.png";
+        localStorage.setItem("theme", "dark")
     }
     else {
         const lightButton = document.querySelector(".light-mode-icon");
         lightButton.src = "/public/night-theme.png";
+        localStorage.setItem("theme", "light")
     }
 
 }
+
+if (localStorage.getItem("theme") == "dark") {
+    toggleTheme()
+}
+
