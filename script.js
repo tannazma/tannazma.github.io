d4b4fcf76f1ac85ca17dc91ad383c0f94754f7f4
tannazma <97254921+tannazma@users.noreply.github.com>
2023-08-14 15:06:12 +0200

    adding scroll sticky menu to be automatocally active on scroll down + style

--- script.js
+++ script.js
@@ -18,4 +18,31 @@ function toggleTheme() {
 if (localStorage.getItem("theme") == "dark") {
     toggleTheme()
 }
+window.addEventListener("scroll", () => {
+    document.querySelector('.active')?.classList.remove('active')
+
+    const journeyOffsetTop = document.getElementById("journey").offsetTop
+    const developmentOffsetTop = document.getElementById("development").offsetTop
+    const seoOffsetTop = document.getElementById("seo").offsetTop
+    const socialOffsetTop = document.getElementById("social").offsetTop
+
+    // for journey
+    if (window.scrollY > journeyOffsetTop && window.scrollY < developmentOffsetTop) {
+        const targetActiveDiv = document.querySelector('a[href="#journey"]').parentElement;
+        targetActiveDiv.classList.add('active')
+    }
+
+    // for seo
+    if (window.scrollY > developmentOffsetTop && window.scrollY < seoOffsetTop) {
+        const targetActiveDiv = document.querySelector('a[href="#development"]').parentElement;
+        targetActiveDiv.classList.add('active')
+    }
+    // for social
+    if (window.scrollY > seoOffsetTop && window.scrollY < socialOffsetTop) {
+        const targetActiveDiv = document.querySelector('a[href="#development"]').parentElement;
+        targetActiveDiv.classList.add('active')
+    }
+
+    console.log(window.scrollY, 'j', journeyOffsetTop, 'd', developmentOffsetTop, 's', seoOffsetTop)
+})
 
